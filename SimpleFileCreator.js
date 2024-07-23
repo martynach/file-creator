1d875d35d43c4624367cc168247bb856db77f161
Martyna Niegowska <DPRW84@motorolasolutions.com>
2024-07-23 23:43:56 +0200

    small changes

---
+++ SimpleFileCreator.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+console.log("***********************************")
+console.log("Dirname: ")
+console.log(__dirname);
+console.log("Filename: ")
+console.log(__filename)
+console.log("***********************************")
+
+if(process.argv.length)
+    var index = 0;
+
+console.log("process.argv.length: ", process.argv.length);
+
+while(index <  process.argv.length) {
+    console.log(`process.argv[${index}]: ${process.argv[index]}`);
+    index++;
+
+}
+
+console.log("***********************************")
+
+
+
+function WriteFile(fileName, message) {
+    fs.writeFile(fileName, message, function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log("File created");
+        console.log("***********************************")
+    });
+}
+
+
+WriteFile("message.txt", "This text should be inside created file!");
+
+
+
